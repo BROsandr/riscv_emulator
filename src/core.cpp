e273371d7afcbd9c5566f4afca4b76b4d0a9f176
alexkharl <sasha_xarlamov@mail.ru>
2024-02-05 12:34:39 +0300

    Ref(core):Выпиливание while (true)

--- src/core.cpp
+++ src/core.cpp
@@ -6,18 +6,15 @@ void Core::cycle() {
   Memory_stage memory_stage{this->data_mem};
   Decoder decoder{};
 
-  while (true) {
-    Xlen instr = fectch_stage.get_instruction();
+  Xlen instr = fectch_stage.get_instruction();
 
-    decoder.decode(instr);
+  decoder.decode(instr);
 
-    Execute_stage execute_stage{docoder.get_unit_select, decoder.get_op, decoder.get_rs1, decoder.get_rs2};
-    execute_stage.execute();
+  Execute_stage execute_stage{docoder.get_unit_select, decoder.get_op, decoder.get_rs1, decoder.get_rs2};
+  execute_stage.execute();
 
-    Csr csr
+  Csr csr
 
-    fectch_stage.calculate_next_pc();
-
-  }
+  fectch_stage.calculate_next_pc();
 
 }
