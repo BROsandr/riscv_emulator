11084f0276c8c602e9b1a20ef8b073efce92a4b9
alexkharl <sasha_xarlamov@mail.ru>
2024-02-06 15:24:21 +0300

    Feat(decoder):Доб-ие extract_bits

--- src/decoder.cpp
+++ src/decoder.cpp
@@ -2,6 +2,33 @@
 
 #include "exception.hpp"
 
+namespace {
+  template <typename T>
+  constexpr T make_mask(std::size_t len, std::size_t pos = 0) {
+    return ((static_cast<T>(1) << len)-1) << pos;
+  }
+
+  template<typename T>
+  concept Right_shiftable = requires(T a) {
+    a >> 1;
+  };
+
+  template<typename T>
+  concept Andable = requires(T a) {
+    a & static_cast<T>(1);
+  };
+
+  template <typename T> requires Right_shiftable<T> && Andable<T>
+  constexpr T extract_bits(T data, std::size_t start_pos, std::size_t end_pos) {
+    return (data >> start_pos) & static_cast<T>(make_mask<unsigned long long>(end_pos-start_pos+1));
+  }
+
+  template <typename T> requires Right_shiftable<T> && Andable<T>
+  constexpr T extract_bits(T data, std::size_t pos) {
+    return extract_bits(data, pos, pos);
+  }
+}
+
 constexpr Decoder::Decoder(Uxlen instruction) {
   const Opcode opcode{static_cast<Opcode>((instruction >> 2) & 0b11111)};
 
