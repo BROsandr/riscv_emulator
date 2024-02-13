d364fce8e8a460ca304c2873de4fb2f591c7e8ec
alexkharl <sasha_xarlamov@mail.ru>
2024-02-14 00:33:25 +0300

    Ref(alu):Доб-ие проверки на диапазон shamt

--- src/alu.cpp
+++ src/alu.cpp
@@ -7,31 +7,35 @@ namespace {
   using enum Alu::Op;
 
   constexpr std::pair<Uxlen, bool> calculate(Alu::Op op, Uxlen a, Uxlen b) {
-      constexpr Uxlen shamt_mask{(sizeof(Uxlen) * 8) - 1};
-      const Uxlen shamt{b & shamt_mask};
       const Sxlen a_signed{static_cast<Sxlen>(a)};
       const Sxlen b_signed{static_cast<Sxlen>(b)};
+
+      const auto get_shamt = [b]() constexpr {
+        constexpr Uxlen shamt_mask{(sizeof(Uxlen) * 8) - 1};
+        assert((b & shamt_mask) == b);
+        return b;
+      };
 
       Uxlen result{};
       bool  flag  {};
 
       switch (op) {
-        case ADD : result = a + b                                ; break;
-        case SUB : result = a - b                                ; break;
-        case XOR : result = a ^ b                                ; break;
-        case OR  : result = a | b                                ; break;
-        case AND : result = a & b                                ; break;
-        case SRA : result = static_cast<Uxlen>(a_signed >> shamt); break;
-        case SRL : result = a >> shamt                           ; break;
-        case SLL : result = a << shamt                           ; break;
-        case SLTS: result = a_signed < b_signed                  ; break;
-        case SLTU: result = a < b                                ; break;
-        case LTS : flag   = a_signed < b_signed                  ; break;
-        case LTU : flag   = a < b                                ; break;
-        case GES : flag   = a_signed >= b_signed                 ; break;
-        case GEU : flag   = a >= b                               ; break;
-        case EQ  : flag   = a == b                               ; break;
-        case NE  : flag   = a != b                               ; break;
+        case ADD : result = a + b                                      ; break;
+        case SUB : result = a - b                                      ; break;
+        case XOR : result = a ^ b                                      ; break;
+        case OR  : result = a | b                                      ; break;
+        case AND : result = a & b                                      ; break;
+        case SRA : result = static_cast<Uxlen>(a_signed >> get_shamt()); break;
+        case SRL : result = a >> get_shamt()                           ; break;
+        case SLL : result = a << get_shamt()                           ; break;
+        case SLTS: result = a_signed < b_signed                        ; break;
+        case SLTU: result = a < b                                      ; break;
+        case LTS : flag   = a_signed < b_signed                        ; break;
+        case LTU : flag   = a < b                                      ; break;
+        case GES : flag   = a_signed >= b_signed                       ; break;
+        case GEU : flag   = a >= b                                     ; break;
+        case EQ  : flag   = a == b                                     ; break;
+        case NE  : flag   = a != b                                     ; break;
         default  : assert((void("Unknown_alu_op " + std::to_string(op)), 0));
       }
 
