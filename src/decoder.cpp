3ea631462aecec3d155af0e72f83249a62758b2a
alexkharl <sasha_xarlamov@mail.ru>
2024-02-24 01:08:16 +0300

    Ref(decoder):Уд-ие conv warning в sing_extend

--- src/decoder.cpp
+++ src/decoder.cpp
@@ -61,7 +61,7 @@ namespace {
   constexpr T sign_extend(T data, std::size_t sign_pos) {
     assert(sign_pos < (sizeof(data) * 8));
 
-    T m{1U << (sign_pos-1)};
+    T m{T{1} << (sign_pos-1)};
     return (data ^ m) - m;
   }
 
