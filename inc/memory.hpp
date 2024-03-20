17ab4de4e55a6dc094f293f9e9773805cb39525f
alexkharl <sasha_xarlamov@mail.ru>
2024-03-20 13:26:23 +0300

    Feat(memory):Доведение mem до заверш сост

--- inc/memory.hpp
+++ inc/memory.hpp
@@ -6,23 +6,21 @@
 
 class Memory {
   public:
-    using Word = Uxlen;
-
-    virtual void write(std::size_t addr, Uxlen data)       = 0;
-    virtual Word read (std::size_t addr)             const = 0;
+    virtual void write(std::size_t addr, Uxlen data, unsigned int byte_en = 0xf) = 0;
+    virtual Uxlen read (std::size_t addr, unsigned int byte_en = 0xf) const = 0;
 
     virtual ~Memory() = default;
 };
 
 class Rf : public Memory {
   public:
-    Rf(std::size_t registers_number = 32) : registers(registers_number, 0) {}
+    Rf(unsigned int registers_number = 32) : registers(registers_number, 0) {}
 
-    void write(std::size_t addr, Uxlen data) override {
-      registers.at(addr) = data;
+    void write(std::size_t addr, Uxlen data, unsigned int byte_en = 0xf) override {
+      registers[addr] = data;
     }
-    Word read (std::size_t addr) const override {
-      return registers.at(addr);
+    Uxlen read (std::size_t addr, unsigned int byte_en = 0xf) const override {
+      return registers[addr];
     }
 
   private:
