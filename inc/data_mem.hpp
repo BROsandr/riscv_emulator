7b1511772db84a589db7875618cd90b94455117a
alexkharl <sasha_xarlamov@mail.ru>
2024-07-17 16:07:23 +0300

    Fix(data_mem):Доб-ие кастов с byte

--- inc/data_mem.hpp
+++ inc/data_mem.hpp
@@ -11,27 +11,33 @@ class Data_mem_wrap : public Memory {
     Data_mem_wrap(Container &container) : m_container{container} {}
 
     void write(std::size_t addr, Uxlen data, unsigned int byte_en = 0xf) override {
+      const auto to_byte = [](Uxlen data_) constexpr {
+        return std::byte{static_cast<uint8_t>(data_)};
+      };
       if (extract_bits(byte_en, 0)) {
-        m_container[addr] = extract_bits(data, {7,0});
+        m_container[addr] = to_byte(extract_bits(data, {7,0}));
       }
       if (extract_bits(byte_en, 1)) {
-        m_container[addr+1] = extract_bits(data, {15,8});
+        m_container[addr+1] = to_byte(extract_bits(data, {15,8}));
       }
       if (extract_bits(byte_en, 2)) {
-        m_container[addr+2] = extract_bits(data, {23,16});
+        m_container[addr+2] = to_byte(extract_bits(data, {23,16}));
       }
       if (extract_bits(byte_en, 3)) {
-        m_container[addr+3] = extract_bits(data, {31,24});
+        m_container[addr+3] = to_byte(extract_bits(data, {31,24}));
       }
     }
 
     Uxlen read (std::size_t addr, unsigned int byte_en = 0xf) override {
       try {
         const auto &const_container{m_container};
-        const Uxlen data =  (const_container.at(addr  ) << CHAR_BIT) |
-                            (const_container.at(addr+1) << CHAR_BIT) |
-                            (const_container.at(addr+2) << CHAR_BIT) |
-                            (const_container.at(addr+3)            );
+        const auto to_uxlen = [](std::byte b) constexpr {
+          return std::to_integer<Uxlen>(b);
+        };
+        const Uxlen data =  (to_uxlen(const_container.at(addr  )) << CHAR_BIT) |
+                            (to_uxlen(const_container.at(addr+1)) << CHAR_BIT) |
+                            (to_uxlen(const_container.at(addr+2)) << CHAR_BIT) |
+                            (to_uxlen(const_container.at(addr+3))            );
         return data;
       } catch (const std::out_of_range&) {
         using std::to_string;
