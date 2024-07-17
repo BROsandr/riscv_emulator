852400b8aa737c65043d3bdbcf5d530703172f71
alexkharl <sasha_xarlamov@mail.ru>
2024-07-18 00:46:59 +0300

    Ref(instr_mem):Доб-ие const к container

--- inc/instr_mem.hpp
+++ inc/instr_mem.hpp
@@ -8,16 +8,16 @@
 
 #include <stdexcept>
 #include <string>
+#include <utility>
 
 template <typename Container> requires requires (Container cont) {
-  { cont[0] } -> std::convertible_to<Uxlen>;
-  { cont.at(0) } -> std::convertible_to<Uxlen>;
+  { std::as_const(cont).at(0) } -> std::convertible_to<Uxlen>;
 }
 class Instr_mem_wrap : public Memory {
   public:
     using value_type = Uxlen;
 
-    Instr_mem_wrap(Container &instr_container) : m_instr_container{instr_container} {}
+    Instr_mem_wrap(const Container &instr_container) : m_instr_container{instr_container} {}
 
     void write(std::size_t addr, Uxlen data, unsigned int byte_en = 0xf) override {
       throw Errors::Read_only{"Write into instr_mem"};
@@ -27,14 +27,17 @@ class Instr_mem_wrap : public Memory {
       assert((byte_en == 0xf) && "only word access is supported");
       assert(!(addr & 0b11) && "only alignment by word is supported");
       const std::size_t word_addr{addr >> 2};
-      try {
-        return m_instr_container.at(word_addr);
-      } catch (const std::out_of_range&) {
-        using std::to_string;
-        throw Errors::Illegal_addr(addr, "requested address is out of instr_mem range.");
-      }
+      return try_get(word_addr);
     }
 
   private:
-    Container &m_instr_container;
+    const Container &m_instr_container;
+
+    constexpr value_type try_get(std::size_t addr) const {
+      try {
+        return std::as_const(m_instr_container).at(addr);
+      } catch (const std::out_of_range&) {
+        throw Errors::Illegal_addr(addr, "read address is out of instr_mem range.");
+      }
+    }
 };
