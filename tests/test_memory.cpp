f253ef7510d0f4c7e2125a455c9b64235b4ca322
alexkharl <sasha_xarlamov@mail.ru>
2024-07-21 14:09:27 +0300

    Ref(tests/mem):Восст-ие проверки контейнера

--- tests/test_memory.cpp
+++ tests/test_memory.cpp
@@ -16,6 +16,7 @@ TEST_CASE("instr mem", "[INSTR_MEM]") {
     std::vector<Uxlen> instr_container{};
     Instr_mem instr_mem{instr_container};
     REQUIRE_THROWS_AS(instr_mem.read(0), Errors::Illegal_addr);
+    REQUIRE(instr_mem.get_content().empty());
   }
 
   SECTION("out_of_range_read") {
@@ -24,6 +25,7 @@ TEST_CASE("instr mem", "[INSTR_MEM]") {
     Instr_mem instr_mem{instr_container};
     REQUIRE_NOTHROW(instr_mem.read(0));
     REQUIRE_THROWS_AS(instr_mem.read(4), Errors::Illegal_addr);
+    REQUIRE(instr_mem.get_content().size() == 1);
   }
 
   SECTION("simple 2 reads") {
@@ -32,12 +34,15 @@ TEST_CASE("instr mem", "[INSTR_MEM]") {
 
     REQUIRE(instr_mem.read(0) == data.at(0));
     REQUIRE(instr_mem.read(4) == data.at(1));
+
+    REQUIRE(instr_mem.get_content() == data);
   }
 
   SECTION("exception") {
     std::vector<Uxlen> instr_container{};
     Instr_mem instr_mem{instr_container};
     REQUIRE_THROWS_AS(instr_mem.write(0, 0), Errors::Read_only);
+    REQUIRE(instr_mem.get_content().empty());
   }
 }
 
@@ -52,6 +57,7 @@ TEST_CASE("data_mem", "[DATA_MEM]") {
       Data_mem data_mem{{}};
       REQUIRE_NOTHROW(data_mem.write(0, 0));
       REQUIRE_THROWS(data_mem.read(4));
+      REQUIRE(data_mem.get_content().size() == 4);
     }
     SECTION("byte_en=0x1") {
       Data_mem data_mem{{}};
@@ -59,12 +65,15 @@ TEST_CASE("data_mem", "[DATA_MEM]") {
       REQUIRE_THROWS(data_mem.read(0, 0b10));
       REQUIRE_THROWS(data_mem.read(0, 0b100));
       REQUIRE_THROWS(data_mem.read(0, 0b1000));
+      REQUIRE(data_mem.get_content().at(0) == Byte{0xff});
+      REQUIRE(data_mem.get_content().size() == 1);
     }
   }
 
   SECTION("uninitialized_read") {
     Data_mem data_mem{{}};
     REQUIRE_THROWS_AS(data_mem.read(0, 0x1), Errors::Illegal_addr);
+    REQUIRE(data_mem.get_content().empty());
   }
 
   SECTION("out_of_range_read") {
@@ -73,6 +82,7 @@ TEST_CASE("data_mem", "[DATA_MEM]") {
     Data_mem data_mem{container};
     REQUIRE_NOTHROW(data_mem.read(0, 0x1));
     REQUIRE_THROWS_AS(data_mem.read(4), Errors::Illegal_addr);
+    REQUIRE(data_mem.get_content().size() == 1);
   }
 
   SECTION("misalignment") {
@@ -84,6 +94,7 @@ TEST_CASE("data_mem", "[DATA_MEM]") {
     Data_mem data_mem{container};
     data_mem.m_assured_aligment = true;
     REQUIRE_THROWS_AS(data_mem.read(0, 0b10000), Errors::Misalignment);
+    REQUIRE(data_mem.get_content().size() == 4);
   }
 
   SECTION("simple 2 instr") {
@@ -103,22 +114,28 @@ TEST_CASE("data_mem", "[DATA_MEM]") {
     SECTION("write") {
       for (unsigned int byte_en{1}; byte_en < (0xf + 1); ++byte_en) {
         SECTION("be=" + std::to_string(byte_en)) {
+          Cont new_data{data};
           Uxlen write_word{};
           std::vector<Byte> test_data{Byte{0xf},Byte{0xf}, Byte{0xf}, Byte{0xf}};
           if (extract_bits(byte_en, 0)) {
+            new_data[0] = test_data[0];
             write_word |= static_cast<Uxlen>(test_data[0]);
           }
           if (extract_bits(byte_en, 1)) {
+            new_data[1] = test_data[1];
             write_word |= static_cast<Uxlen>(test_data[1]) << CHAR_BIT;
           }
           if (extract_bits(byte_en, 2)) {
+            new_data[2] = test_data[2];
             write_word |= static_cast<Uxlen>(test_data[2]) << 2 * CHAR_BIT;
           }
           if (extract_bits(byte_en, 3)) {
+            new_data[3] = test_data[3];
             write_word |= static_cast<Uxlen>(test_data[3]) << 3 * CHAR_BIT;
           }
           data_mem.write(0, write_word, byte_en);
           REQUIRE(data_mem.read(0, byte_en) == write_word);
+          REQUIRE(data_mem.get_content() == new_data);
         }
       }
     }
@@ -142,28 +159,35 @@ TEST_CASE("data_mem", "[DATA_MEM]") {
           REQUIRE(data_mem.read(0, byte_en) == read_word);
           REQUIRE_NOTHROW(data_mem.read(4, byte_en));
           REQUIRE_THROWS(data_mem.read(8, byte_en));
+          REQUIRE(data_mem.get_content() == data);
         }
       }
     }
     SECTION("write-read") {
       for (unsigned int byte_en{1}; byte_en < (0xf + 1); ++byte_en) {
         SECTION("be=" + std::to_string(byte_en)) {
+          Cont new_data{data};
           Uxlen write_word{};
           std::vector<Byte> test_data{Byte{0xf},Byte{0xf}, Byte{0xf}, Byte{0xf}};
           if (extract_bits(byte_en, 0)) {
+            new_data[0] = test_data[0];
             write_word |= static_cast<Uxlen>(test_data[0]);
           }
           if (extract_bits(byte_en, 1)) {
+            new_data[1] = test_data[1];
             write_word |= static_cast<Uxlen>(test_data[1]) << CHAR_BIT;
           }
           if (extract_bits(byte_en, 2)) {
+            new_data[2] = test_data[2];
             write_word |= static_cast<Uxlen>(test_data[2]) << 2 * CHAR_BIT;
           }
           if (extract_bits(byte_en, 3)) {
+            new_data[3] = test_data[3];
             write_word |= static_cast<Uxlen>(test_data[3]) << 3 * CHAR_BIT;
           }
           data_mem.write(0, write_word, byte_en);
           REQUIRE(data_mem.read(0, byte_en) == write_word);
+          REQUIRE(data_mem.get_content() == new_data);
         }
       }
     }
@@ -179,12 +203,14 @@ TEST_CASE("ranged_mem", "[RANGED_MEM]") {
     Ranged_mem_span ranged_cont{data_mem, 4, 11 - 4 + 1};
     REQUIRE_NOTHROW(ranged_cont.write(4, 0));
     REQUIRE_THROWS_AS(ranged_cont.read(8), Errors::Illegal_addr);
+    REQUIRE(data_mem.get_content().size() == 4);
   }
 
   SECTION("uninitialized_read") {
     Data_mem data_mem{{}};
     Ranged_mem_span ranged_cont{data_mem, 4, 11 - 4 + 1};
     REQUIRE_THROWS_AS(ranged_cont.read(4, 0x1), Errors::Illegal_addr);
+    REQUIRE(data_mem.get_content().empty());
   }
 
   SECTION("out_of_range") {
@@ -203,6 +229,7 @@ TEST_CASE("ranged_mem", "[RANGED_MEM]") {
     REQUIRE_THROWS_AS(ranged_cont.read(12, 0xf), Errors::Illegal_addr);
     REQUIRE(ranged_cont.read(4) == 0x04030201);
     REQUIRE(ranged_cont.read(8) == 0x08070605);
+    REQUIRE(data_mem.get_content().size() == 8);
   }
 
   SECTION("throw_when_read_before_write") {
@@ -217,6 +244,15 @@ TEST_CASE("ranged_mem", "[RANGED_MEM]") {
     REQUIRE_THROWS_AS(ranged_cont.read(8, 0xf), Errors::Illegal_addr);
     ranged_cont.write(8, 0x08070605, 0xf);
     REQUIRE(ranged_cont.read(8, 0xf) == 0x08070605);
+    REQUIRE(data_mem.get_content().at(0) == Byte{1});
+    REQUIRE(data_mem.get_content().at(1) == Byte{2});
+    REQUIRE(data_mem.get_content().at(2) == Byte{3});
+    REQUIRE(data_mem.get_content().at(3) == Byte{4});
+    REQUIRE(data_mem.get_content().at(4) == Byte{5});
+    REQUIRE(data_mem.get_content().at(5) == Byte{6});
+    REQUIRE(data_mem.get_content().at(6) == Byte{7});
+    REQUIRE(data_mem.get_content().at(7) == Byte{8});
+    REQUIRE(data_mem.get_content().size() == 8);
   }
 
   SECTION("misalignment") {
@@ -229,6 +265,6 @@ TEST_CASE("ranged_mem", "[RANGED_MEM]") {
     Ranged_mem_span ranged_cont{data_mem, 4, 11 - 4 + 1};
     data_mem.m_assured_aligment = true;
     REQUIRE_THROWS_AS(ranged_cont.read(4, 0b10000), Errors::Misalignment);
-    REQUIRE(container.size() == 4);
+    REQUIRE(data_mem.get_content().size() == 4);
   }
 }
