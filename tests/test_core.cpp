0c26157daa6550e96f51ea45ee6a21c29b6fbf6e
alexkharl <sasha_xarlamov@mail.ru>
2024-07-25 01:11:38 +0300

    Feat(tests/core):Доб-ие проверки содерж памятей

--- tests/test_core.cpp
+++ tests/test_core.cpp
@@ -16,6 +16,24 @@
 
 #include <map>
 
+template <typename D, typename R, typename C>
+class Mem_requirements {
+  public:
+    Mem_requirements(const D &data_mem, const R &rf, const C &csr)
+        : m_data_mem{data_mem}, m_rf{rf}, m_csr{csr} {}
+
+    void eq(auto &ref_data_content, auto &ref_rf_content, auto &ref_csr_content) const {
+      REQUIRE(m_data_mem.get_content() == ref_data_content);
+      REQUIRE(m_rf.get_content()       == ref_rf_content  );
+      REQUIRE(m_csr.get_content()      == ref_csr_content );
+    }
+
+  private:
+    const D &m_data_mem;
+    const R &m_rf;
+    const C &m_csr;
+};
+
 TEST_CASE("basic", "[BASIC]") {
 
   auto sink = std::make_shared<spdlog::sinks::stdout_color_sink_st>();
@@ -34,9 +52,14 @@ TEST_CASE("basic", "[BASIC]") {
     Traced_mem_wrap traced_rf{rf, my_logger, "Rf"};
     Csr csr{};
     Traced_mem_wrap traced_csr{csr, my_logger, "Csr"};
+    auto ref_rf_content = rf.get_content();
+    auto ref_csr_content = csr.get_content();
+    auto ref_data_content = data_mem.get_content();
 
     Core core{traced_instr_mem, data_mem, traced_csr, traced_rf,
         my_logger, extensions};
+
+    Mem_requirements mem_reqs{data_mem, rf, csr};
 
     try {
       core.cycle();
@@ -46,6 +69,7 @@ TEST_CASE("basic", "[BASIC]") {
       my_logger->critical("Unhandled exception");
     }
 
-    REQUIRE(rf.get_content()[1] == 1);
+    ref_rf_content[1] = 1;
+    mem_reqs.eq(ref_data_content, ref_rf_content, ref_csr_content);
   }
 }
